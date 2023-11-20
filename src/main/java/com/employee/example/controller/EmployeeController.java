43d2127e41e8a932f849d7ecba6c1270dff55e48
Anas Naciri <110792944+Naciri-Anas@users.noreply.github.com>
2023-11-20 11:42:28 +0100

    Update EmployeeController.java

--- src/main/java/com/employee/example/controller/EmployeeController.java
+++ src/main/java/com/employee/example/controller/EmployeeController.java
@@ -1,13 +1,28 @@
 package com.employee.example.controller;
 
+import com.employee.example.beans.Employee;
 import com.employee.example.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
-    
+
+    @GetMapping("/")
+    public String viewHomePage(Model model){
+        model.addAttribute("allemplist",employeeService.getAllEmployee());
+        return "index";
+
+    }
+    @GetMapping("/add")
+    public String AddNewEmployee(Model model){
+        Employee employee =  new Employee();
+        model.addAttribute("employee",employee);
+        return "employee";
+    }
 
 }
