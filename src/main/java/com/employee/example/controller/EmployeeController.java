da39d4ffca0a39ee9730d18450ecf3584781d10c
Anas Naciri <110792944+Naciri-Anas@users.noreply.github.com>
2023-11-20 11:45:16 +0100

    Update EmployeeController.java

--- src/main/java/com/employee/example/controller/EmployeeController.java
+++ src/main/java/com/employee/example/controller/EmployeeController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class EmployeeController {
@@ -24,5 +26,11 @@ public class EmployeeController {
         model.addAttribute("employee",employee);
         return "employee";
     }
+    @PostMapping("/save")
+    public String SaveEmployee (@ModelAttribute("employee") Employee employee){
+        employeeService.save(employee);
+        return "redirect :/";
 
+
+    }
 }
