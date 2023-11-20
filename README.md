0c92883c4de6d7df8bfe8dc81e90cb48b1b57b93
Anas Naciri <110792944+Naciri-Anas@users.noreply.github.com>
2023-11-20 09:56:17 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# EmployeeManagement
+ 
---
+++ src/test/java/com/employee/example/EmployeeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employee.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
